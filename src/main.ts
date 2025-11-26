665c45671edcb238731bbd698503f9553f4d777a
Alexandre Cordeiro <alexandre.13a@gmail.com>
2025-11-26 12:57:45 -0300

    261120251

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
 
   const isProd = process.env.NODE_ENV === 'production';
   const port = isProd ? 4997 : 4997;
-  const host = '192.168.1.169';
+  //const host = '192.168.1.169';
+  const host = '192.168.0.169';
 
   await app.listen(port, host, () => {
     console.log('\x1b[32m%s\x1b[0m', `✅ Server running on IP ${host} and listening on port ${port} (${isProd ? 'PROD' : 'DEV'})`);
